6675f63457b14bb0834074c3caf607c1e23dfbf0
Amiya Vatsa <97239839+AmiyaVatsa@users.noreply.github.com>
2023-12-28 14:42:56 +0530

    Update app.py

--- India_Livestock/app.py
+++ India_Livestock/app.py
@@ -1,15 +1,11 @@
-import os
-
-from cs50 import SQL
 from flask import Flask, render_template
-from tempfile import mkdtemp
-
+import sqlite3
 
 app = Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-db = SQL("sqlite:///livestocks.db")
+connection = sqlite3.connect('livestocks.db')
+db = connection.cursor()
 
 @app.route("/")
 def index():
@@ -22,12 +18,12 @@ def about_us():
 @app.route("/district.html")
 def districts():
     data = db.execute("SELECT state_name as STATE, district_name as DISTRICT, cattle as CATTLE, buffalo as BUFFALO, sheep as SHEEP, goat as GOAT, horse as HORSE, pony AS PONY, mule AS MULE, donkey as DONKEY, camel AS CAMEL, total_poultry AS POULTRY FROM livestocks GROUP BY district_name ORDER BY state_name")
-    return render_template("/district.html", data=data)
+    return render_template("/district.html", data=dict(data.fetchall()))
 
 @app.route("/state.html")
 def states():
     data = db.execute("SELECT state_name as STATE, district_name as DISTRICT, SUM(cattle) as CATTLE, SUM(buffalo) as BUFFALO, SUM(sheep) as SHEEP, SUM(goat) as GOAT, SUM(horse) as HORSE, SUM(pony) AS PONY, SUM(mule) AS MULE, SUM(donkey) as DONKEY, SUM(camel) AS CAMEL, SUM(total_poultry) AS POULTRY FROM livestocks GROUP BY state_name ORDER BY state_name")
-    return render_template("/state.html", data=data)
+    return render_template("/state.html", data=dict(data.fetchall()))
 
 
 
