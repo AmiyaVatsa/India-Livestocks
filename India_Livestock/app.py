281aa199cb893e573d9093b9090bb763448d15af
Amiya Vatsa <97239839+AmiyaVatsa@users.noreply.github.com>
2023-12-28 14:33:25 +0530

    Update app.py

--- India_Livestock/app.py
+++ India_Livestock/app.py
@@ -1,7 +1,7 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, render_template
 from tempfile import mkdtemp
 
 
